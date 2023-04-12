e0d930ff8721e49dcd8766737f5a37bac142b369
kohshi54 <kohshi54.yam@gmail.com>
2023-04-12 17:35:59 +0900

    add my_read()

--- get_next_line.c
+++ get_next_line.c
@@ -1,60 +1,65 @@
 #include "get_next_line.h"
 
+size_t my_read(int fd, char *buf, char *line)
+{
+	ssize_t     read_byte;
+
+	read_byte = read(fd, buf, BUFFER_SIZE);
+	if (read_byte == 0)
+		return (0);
+	else if (read_byte == -1)
+	{
+		free(line);
+		return (0);
+	}
+	buf[read_byte] = '\0';
+	return (read_byte);
+}
 
 char *get_next_line(int fd)
 {
-   char        buf[BUFFER_SIZE + 1];
-   char        *line;
-   static char *save;
-   ssize_t     read_byte;
-   size_t      byte_to_nl;
-   char        *tmp;
+	char        buf[BUFFER_SIZE + 1];
+	char        *line;
+	static char *save;
+	size_t      byte_to_nl;
+	char        *tmp;
 
-
-   line = NULL;
-   if (save)
-   {
-       byte_to_nl = ft_strchr_nl(save);
-       if (byte_to_nl)
-       {
-           line = ft_strnjoin(line, save, byte_to_nl);
-           tmp = save;
-           save = ft_strdup(&save[byte_to_nl]);
-           free(tmp);
-           return (line);
-       }
-       else
-       {
-           tmp = ft_strnjoin(line, save, ft_strlen(save));
-           free(line);
-           line = tmp;
-           free(save);
-           save = NULL;
-       }
-   }
-   while (1)
-   {
-       read_byte = read(fd, buf, BUFFER_SIZE);
-       if (read_byte == 0)
-           break ;
-       else if (read_byte == -1)
-           return (NULL);
-       buf[read_byte] = '\0';
-       byte_to_nl = ft_strchr_nl(buf);
-       if (byte_to_nl)
-       {
-           save = ft_strdup(&buf[byte_to_nl]);
-           tmp = ft_strnjoin(line, buf, byte_to_nl);
-           free(line);
-           line = tmp;
-           return (line);
-       }
-       else
-       {
-           tmp = ft_strnjoin(line, buf, BUFFER_SIZE);
-           free(line);
-           line = tmp;
-       }
-   }
-   return (line);
+	line = NULL;
+	if (save)
+	{
+		byte_to_nl = ft_strchr_nl(save);
+		if (byte_to_nl)
+		{
+			line = ft_strnjoin(line, save, byte_to_nl);
+			tmp = save;
+			save = ft_strdup(&save[byte_to_nl]);
+			free(tmp);
+			return (line);
+		}
+		else
+		{
+			line = ft_strnjoin(line, save, ft_strlen(save));
+			free(save);
+			save = NULL;
+		}
+	}
+	while (my_read(fd, buf, line))
+	{
+		byte_to_nl = ft_strchr_nl(buf);
+		if (byte_to_nl)
+		{
+			save = ft_strdup(&buf[byte_to_nl]);
+			tmp = ft_strnjoin(line, buf, byte_to_nl);
+			free(line);
+			line = tmp;
+			return (line);
+		}
+		else
+		{
+			tmp = ft_strnjoin(line, buf, BUFFER_SIZE);
+			free(line);
+			line = tmp;
+		}
+	}
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-
 #include <stdlib.h>
 #include <unistd.h>
 
 char    *get_next_line(int fd);
 size_t	ft_strlen(const char *s);
-char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 size_t	ft_strchr_nl(const char *s);
 char    *ft_strnjoin(char const *s1, char const *s2, size_t n);
@@ -15,6 +13,5 @@ char    *ft_strnjoin(char const *s1, char const *s2, size_t n);
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
 #endif
-
 
 #endif
